04ee060d814035df0e8c71ef14be5fa13f170667
BANGBANGROX <lakshya0809bang@gmail.com>
2022-05-29 16:49:51 +0530

    Minor changes for deployment

---
+++ dist/index.46ca9997.js
@@ -0,0 +1,2 @@
+const btn=document.getElementById("menu-btn"),nav=document.getElementById("menu");btn.addEventListener("click",(()=>{console.log("Click"),btn.classList.toggle("open"),nav.classList.toggle("flex"),nav.classList.toggle("hidden")}));
+//# sourceMappingURL=index.46ca9997.js.map
